e6dec53ab15394b2c313fac3f0c6b9a6755cdfb4
jademon <j2python@gmail.com>
2023-04-03 16:29:33 +0900

    add validation check

--- src/main/java/com/example/nettalk/entity/room/Room.java
+++ src/main/java/com/example/nettalk/entity/room/Room.java
@@ -5,6 +5,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
 
 @Data
 @Entity
@@ -18,15 +19,18 @@ public class Room {
     private Long id;
 
     @Column(name = "title")
+    @NotBlank(message = "방제목을 입력해주세요.")
     private String title;
 
     @Column(name = "comment")
+    @NotBlank(message = "방 소개를 입력해주세요.")
     private String comment;
 
     @Column(name = "user_count")
     private String userCount;
 
     @Column(name = "max_count")
+    @NotBlank(message = "최대 인원을 입력해주세요.")
     private String maxCount;
 
 }
