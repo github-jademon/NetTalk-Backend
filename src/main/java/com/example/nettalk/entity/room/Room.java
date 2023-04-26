bebdc4974ff20b52e6f5f2e92781a3cd7e5650df
jademon <j2python@gmail.com>
2023-04-26 19:37:34 +0900

    [FEAT] add member, chatmessage to room entity

--- src/main/java/com/example/nettalk/entity/room/Room.java
+++ src/main/java/com/example/nettalk/entity/room/Room.java
@@ -1,11 +1,16 @@
 package com.example.nettalk.entity.room;
 
+import com.example.nettalk.entity.chat.ChatMessage;
+import com.example.nettalk.entity.member.Member;
+import com.example.nettalk.entity.user_room.UserRoom;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
+import java.util.List;
 
 @Data
 @Entity
@@ -18,6 +23,9 @@ public class Room {
     @Column(name = "id")
     private Long id;
 
+    @OneToOne
+    private Member owner;
+
     @Column(name = "title")
     @NotBlank(message = "방제목을 입력해주세요.")
     private String title;
@@ -27,10 +35,25 @@ public class Room {
     private String comment;
 
     @Column(name = "user_count")
-    private String userCount;
+    private int userCount;
 
     @Column(name = "max_count")
-    @NotBlank(message = "최대 인원을 입력해주세요.")
-    private String maxCount;
+//    @NotBlank(message = "최대 인원을 입력해주세요.")
+    private int maxCount;
+
+    @OneToMany(mappedBy = "room")
+    private List<UserRoom> member;
+
+    @OneToMany
+    private List<ChatMessage> chatMessages;
+
+    @Builder
+    public Room(Member owner, String title, String comment, int maxCount) {
+        this.owner = owner;
+        this.title = title;
+        this.comment = comment;
+        this.userCount = 1;
+        this.maxCount = maxCount;
+    }
 
 }
--- src/main/java/com/example/nettalk/service/RoomService.java
+++ src/main/java/com/example/nettalk/service/RoomService.java
@@ -1,29 +1,133 @@
 package com.example.nettalk.service;
 
+import com.example.nettalk.dto.memberRoom.MemberRoomUpdateRequestDto;
+import com.example.nettalk.entity.chat.ChatMessage;
+import com.example.nettalk.entity.member.Member;
+import com.example.nettalk.entity.member.MemberRepository;
 import com.example.nettalk.entity.room.Room;
 import com.example.nettalk.entity.room.RoomRepository;
+import com.example.nettalk.entity.user_room.UserRoom;
+import com.example.nettalk.entity.user_room.UserRoomRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
 
-@Transactional(readOnly = true)
+@Transactional
 @Service
 @RequiredArgsConstructor
 public class RoomService {
     private final RoomRepository roomRepository;
+    private final MemberRepository memberRepository;
+    private final UserRoomRepository userRoomRepository;
 
-    public List<Room> list() {
-        return roomRepository.findAll();
+    public ResponseEntity list() {
+        try {
+            List<Room> data = roomRepository.findAll();
+
+            return new ResponseEntity(data, HttpStatus.OK);
+        } catch(Exception e) {
+            e.printStackTrace();
+            return new ResponseEntity<>(HttpStatus.OK);
+        }
     }
 
-    public Optional<Room> getRoom(Long id) {
-        return roomRepository.findById(id);
+    public ResponseEntity getRoom(Long userId, Long roomId) {
+        try {
+            HashMap<String, Object> data = new HashMap<>();
+            Room room = roomRepository.findById(roomId).get();
+            data.put("room", new Room().builder().comment(room.getComment()).title(room.getTitle()).owner(room.getOwner()).build());
+            if(userId!=null) {
+                data.put("messages", this.update(userId, roomId));
+                Member member = memberRepository.findById(userId).get();
+                String username = userRoomRepository.findByMemberAndRoom(member, room).get().getUsername();
+                Map<String, String> user = new HashMap<>();
+                user.put("email", member.getEmail());
+                user.put("name", username);
+                data.put("user", user);
+            }
+
+            return new ResponseEntity(data, HttpStatus.OK);
+        } catch(Exception e) {
+            e.printStackTrace();
+            return new ResponseEntity<>(HttpStatus.OK);
+        }
     }
 
-    public void createRoom(Room room) {
-        roomRepository.save(room);
+    public ResponseEntity createRoom(Long userid, Room res) {
+        try {
+            if(userid==null) throw new Exception();
+            Member owner = memberRepository.findById(userid).get();
+            Room room = roomRepository.save(new Room().builder()
+                            .owner(owner)
+                            .title(res.getTitle())
+                            .comment(res.getComment())
+                            .maxCount(res.getMaxCount())
+                            .build());
+
+            userRoomRepository.save(
+                    new UserRoom()
+                            .builder()
+                            .room(room)
+                            .member(owner)
+                            .build());
+
+            return new ResponseEntity(HttpStatus.OK);
+        } catch(Exception e) {
+            e.printStackTrace();
+            return new ResponseEntity(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    public void updateUserRoom(Long id, MemberRoomUpdateRequestDto memberRoomUpdateRequestDto) {
+        try {
+            System.out.println(memberRoomUpdateRequestDto.getEmail());
+
+            Member member = memberRepository.findByEmail(memberRoomUpdateRequestDto.getEmail()).get();
+            Room room = roomRepository.findById(id).get();
+            UserRoom userRoom = userRoomRepository.findByMemberAndRoom(member, room).get();
+
+            System.out.println(memberRoomUpdateRequestDto.getName());
+
+            userRoom.setUsername(memberRoomUpdateRequestDto.getName());
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void addUserRoom(Member member, Room room, String name) {
+        room.setUserCount(room.getUserCount()+1);
+        userRoomRepository.save(
+                new UserRoom()
+                        .builder()
+                        .username(name)
+                        .room(room)
+                        .member(member)
+                        .build());
+    }
+
+    public List update(Long userId, Long roomId) {
+        Member member = memberRepository.findById(userId).get();
+        Room room = roomRepository.findById(roomId).orElse(null);
+        Optional<UserRoom> userRoom = userRoomRepository.findByMemberAndRoom(member, room);
+
+        if(userRoom.isEmpty()) {
+            addUserRoom(member, room, "");
+            return null;
+        } else {
+            List<ChatMessage> chatMessages = new ArrayList<>();
+
+            for(ChatMessage message:room.getChatMessages()) {
+                System.out.println(message.getDate()+" "+userRoom.get().getJoinAt());
+                if(message.getDate().isAfter(userRoom.get().getJoinAt())) {
+                    chatMessages.add(message);
+                }
+            }
+
+            return chatMessages;
+        }
     }
 }
