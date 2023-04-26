160f6516e2e0559f89f9e34d508f8a6a5131789f
jademon <j2python@gmail.com>
2023-04-26 19:38:12 +0900

    [FEAT] add room to member entity

--- src/main/java/com/example/nettalk/entity/member/Member.java
+++ src/main/java/com/example/nettalk/entity/member/Member.java
@@ -2,9 +2,15 @@ package com.example.nettalk.entity.member;
 
 import com.example.nettalk.entity.authority.Authority;
 import javax.persistence.*;
+
+import com.example.nettalk.entity.room.Room;
+import com.example.nettalk.entity.user_room.UserRoom;
 import lombok.*;
 
+import java.util.List;
+
 @Getter
+@Setter
 @Entity
 @NoArgsConstructor
 @Table(name="users")
@@ -22,11 +28,15 @@ public class Member {
     @Enumerated(EnumType.STRING)
     private Authority authority;
 
+    @OneToMany(mappedBy = "member")
+    private List<UserRoom> room;
+
     @Builder
-    public Member(String email, String userid, String password, Authority authority) {
+    public Member(String email, String userid, String password, Authority authority, List room) {
         this.email = email;
         this.userid = userid;
         this.password = password;
         this.authority = authority;
+        this.room = room;
     }
 }
